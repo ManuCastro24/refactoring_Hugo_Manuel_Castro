85ff1c40831cd4f05d70c852d4aaf74a033246f6
Manuel Castro <manuelcastrofernandez.mcf@gmail.com>
2024-01-22 18:21:05 +0100

    He modificado el nombre de las variables y colocado de forma optima todo el código.

--- src/refactoring/Ejercicio4.java
+++ src/refactoring/Ejercicio4.java
@@ -1,10 +1,29 @@
-														package refactoring;
-												   import java.util.Scanner;
-												   public class Ejercicio4 {
-public static void main(String[] args) {Scanner sc = new Scanner(System.in);
-System.out.println("Escribe una frase:");
-String LoCuRa = sc.nextLine();String NoLoCuRa = "";
-int numLetras = LoCuRa.length();
-			 for (int nUmErItO = numLetras - 1; nUmErItO >= 0; nUmErItO--) {
-				 	   NoLoCuRa = NoLoCuRa  + "" + LoCuRa.charAt(nUmErItO);}
-			 					  System.out.println(NoLoCuRa);sc.close();}}
+package refactoring;
+
+import java.util.Scanner;
+												   
+public class Ejercicio4 {
+	
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("Escribe una frase:");
+	String frase = sc.nextLine();
+	
+	String Espacio = "";
+	
+	int numLetras = frase.length();
+	
+	for (int nUmErItO = numLetras - 1; nUmErItO >= 0; nUmErItO--) {
+		
+			Espacio = Espacio  + "" + frase.charAt(nUmErItO);
+			
+	}
+	
+	System.out.println(Espacio);
+	
+	sc.close();
+	
+  }
+}
